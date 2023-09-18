8bd677f021199c976fc956ab488f92f6bf817537
Kavin Sitsabeshon <01kavin01@gmail.com>
2023-09-18 16:45:43 -0500

    Bringing all scoring values together

--- src/navigation/navigation.cc
+++ src/navigation/navigation.cc
@@ -231,8 +231,8 @@ float Navigation::GetOptimalCurvature(float angleIncrement){
 }
 
 float Navigation::GetPathScore(float curvature){
-  const float weight_1 = 1;
-  const float weight_2 = 0;//.52;
+  const float weight_1 = 0.5;
+  const float weight_2 = 0.4;//.52;
   const float weight_3 = 0.1;//.15;
 
   return GetFreePathLength(curvature) * weight_1 + ClearanceComputation(curvature) * weight_2 + GetClosestPointOfApproach(curvature) * weight_3;
