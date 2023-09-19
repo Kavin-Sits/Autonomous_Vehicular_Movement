e524ed91ace8e219222d76d16ae58eaea838bc1d
Kavin Sitsabeshon <01kavin01@gmail.com>
2023-09-18 20:02:53 -0500

    reducing clearance

--- src/navigation/navigation.cc
+++ src/navigation/navigation.cc
@@ -224,7 +224,7 @@ float Navigation::GetOptimalCurvature(float angleIncrement){
 
 float Navigation::GetPathScore(float curvature){
   const float weight_1 = 1;
-  const float weight_2 = 0.2;//.52;
+  const float weight_2 = 0.05;//.52;
   const float weight_3 = 0;//0.05;//.15;
 
   return GetFreePathLength(curvature) * weight_1 + ClearanceComputation(curvature) * weight_2 + GetClosestPointOfApproach(curvature) * weight_3;
