d9214994930fbc05da491304711ca343f25f3394
Kavin Sitsabeshon <01kavin01@gmail.com>
2023-09-18 20:19:28 -0500

    preffing straight

--- src/navigation/navigation.cc
+++ src/navigation/navigation.cc
@@ -204,7 +204,7 @@ float Navigation::InstantaneousTimeDecision(){
 float Navigation::GetOptimalCurvature(float angleIncrement){
   float highestScore = -1 * __FLT_MAX__;
   float bestCurvature = 0.0;
-  for(float i=1.0; i>=0.0; i-=angleIncrement){
+  for(float i=0.0; i<=1.0; i+=angleIncrement){
     float currentScore = GetPathScore(i);
     float currentOppositeScore = GetPathScore(-1.0 * i);
     if(currentScore>highestScore){
